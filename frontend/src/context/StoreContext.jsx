8fe350930359b9a2023342ddb66e26f9044393b1
maryam magdy <112756804+Maryam289@users.noreply.github.com>
2026-08-14 22:27:29 +0300

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -11,8 +11,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const[cartItems, setCartItems] = useState({});
-    // const url = "https://website-perfumes-shopping-backend.onrender.com"
-    const url = "http://localhost:4000"
+    const url = "https://website-perfumes-shopping-backend.onrender.com"
+    // const url = "http://localhost:4000"
     const [token, setToken] = useState("");
     const [perfume_list, setPerfumeList] = useState([])
     
